c8328107d8126479b02596d9d0b7a0069145ebfe
Miguel G. Sanguino <sanguino@me.com>
2021-02-07 08:22:33 +0100

    PlaneRepository

---
+++ src/main/java/es/urjc/code/repositories/PlaneRepository.java
@@ -0,0 +1,13 @@
+package es.urjc.code.repositories;
+
+import es.urjc.code.entities.Plane;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repositorio para Plane.
+ *
+ *  @author Miguel G. Sanguino
+ */
+public interface PlaneRepository extends JpaRepository<Plane, Long> {
+
+}
