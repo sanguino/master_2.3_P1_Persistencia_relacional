b06a501227fb9317e7d78e91c26d6455e1576104
Miguel G. Sanguino <sanguino@me.com>
2021-02-07 08:22:18 +0100

    Flight

---
+++ src/main/java/es/urjc/code/entities/Flight.java
@@ -0,0 +1,139 @@
+package es.urjc.code.entities;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Entity Flight.
+ *
+ * @author Miguel G. Sanguino
+ */
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String code;
+    private String company;
+
+    @OneToOne(cascade=CascadeType.ALL)//, mappedBy="flight")
+    private Plane plane;
+
+    @OneToOne(cascade=CascadeType.ALL)//, mappedBy="flight")
+    private Airport originAirport;
+
+    @OneToOne(cascade=CascadeType.ALL)//), mappedBy="flight")
+    private Plane destinationAirport;
+
+    private LocalDateTime departureDateTime;
+
+    private Duration duration;
+
+    @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<CrewMember> crew;
+
+
+
+    public Flight() {
+    }
+
+    public Flight(String code, String company, Plane plane, Airport originAirport, Plane destinationAirport, LocalDateTime departureDateTime, Duration duration) {
+        this.code = code;
+        this.company = company;
+        this.plane = plane;
+        this.originAirport = originAirport;
+        this.destinationAirport = destinationAirport;
+        this.departureDateTime = departureDateTime;
+        this.duration = duration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public Airport getOriginAirport() {
+        return originAirport;
+    }
+
+    public void setOriginAirport(Airport originAirport) {
+        this.originAirport = originAirport;
+    }
+
+    public Plane getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(Plane destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public LocalDateTime getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(LocalDateTime departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public List<CrewMember> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(List<CrewMember> crew) {
+        this.crew = crew;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", company='" + company + '\'' +
+                ", plane=" + plane +
+                ", originAirport=" + originAirport +
+                ", destinationAirport=" + destinationAirport +
+                ", departureDateTime=" + departureDateTime +
+                ", duration=" + duration +
+                ", crew=" + crew +
+                '}';
+    }
+}
