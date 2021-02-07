a748bcea478dd4c82be1c42dd851edd26cb74214
Miguel G. Sanguino <sanguino@me.com>
2021-02-07 10:04:50 +0100

    FlightSaves

--- src/main/java/es/urjc/code/DatabaseLoader.java
+++ src/main/java/es/urjc/code/DatabaseLoader.java
@@ -1,20 +1,23 @@
 package es.urjc.code;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
 import es.urjc.code.entities.Airport;
 import es.urjc.code.entities.CrewMember;
+import es.urjc.code.entities.Flight;
 import es.urjc.code.entities.Plane;
 import es.urjc.code.repositories.AirportRepository;
 import es.urjc.code.repositories.CrewMemberRepository;
+import es.urjc.code.repositories.FlightRepository;
 import es.urjc.code.repositories.PlaneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
+
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Cargador de la BD y ejemplos de consulta.
@@ -33,6 +36,9 @@ public class DatabaseLoader implements CommandLineRunner {
     @Autowired
     private CrewMemberRepository crewMemberRepository;
 
+    @Autowired
+    private FlightRepository flightRepository;
+
     @Override
     public void run(String... args) throws ParseException {
 
@@ -42,15 +48,23 @@ public class DatabaseLoader implements CommandLineRunner {
         planeRepository.save(p1);
         planeRepository.save(p2);
 
-        Airport a1 = new Airport("MAD", "Barajas", "Madrid", "Spain");
-        Airport a2 = new Airport("MUC", "Munich Airport", "Munich", "Germany");
-        airportRepository.save(a1);
-        airportRepository.save(a2);
+        Airport mad = new Airport("MAD", "Barajas", "Madrid", "Spain");
+        Airport muc = new Airport("MUC", "Franz Josef Strauss", "Munich", "Germany");
+        airportRepository.save(mad);
+        airportRepository.save(muc);
 
         CrewMember c1 = new CrewMember("hy76rf", "Jonh", "Smith", "Captain", "Lufthansa");
         CrewMember c2 = new CrewMember("uj87fn", "Jane", "Brown", "Co-pilot", "Lufthansa");
         crewMemberRepository.save(c1);
         crewMemberRepository.save(c2);
+
+        Flight f1 = new Flight("LH9851", " Lufthansa ", p1, mad, muc, LocalDateTime.of(2016, Month.APRIL, 20, 06, 30), Duration.ofHours(2).plusMinutes(35));
+        f1.setCrew(Arrays.asList(c1, c2));
+        flightRepository.save(f1);
+
+        System.out.println("");
+        System.out.println("Guardado completado");
+        System.out.println("");
 
         // Recupera aviones
         List<Plane> planes = planeRepository.findAll();
