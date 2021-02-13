67b633b249ec49b0520428ad35a96d982706f5c0
Miguel G. Sanguino <sanguino@me.com>
2021-02-13 14:25:04 +0100

    add crewListId to flight

---
+++ src/main/resources/db/migration/V2__addCrewJson.sql
@@ -0,0 +1,9 @@
+ALTER TABLE flight
+ADD crew_list_id JSON;
+
+UPDATE flight SET crew_list_id = '[15, 16]' WHERE id = 9;
+UPDATE flight SET crew_list_id = '[17, 18]' WHERE id = 10;
+UPDATE flight SET crew_list_id = '[15, 19]' WHERE id = 11;
+UPDATE flight SET crew_list_id = '[16, 19]' WHERE id = 12;
+UPDATE flight SET crew_list_id = '[17, 18]' WHERE id = 13;
+UPDATE flight SET crew_list_id = '[16, 18]' WHERE id = 14;
