4da57cb45ab0e15a782855df6895260ee8b5d0f9
Miguel G. Sanguino <sanguino@me.com>
2021-02-13 15:14:31 +0100

    add not null to json data

--- src/main/resources/db/migration/V2__addCrewJson.sql
+++ src/main/resources/db/migration/V2__addCrewJson.sql
@@ -1,5 +1,5 @@
 ALTER TABLE flight
-ADD crew_list_id JSON;
+ADD crew_list_id JSON NOT NULL;
 
 UPDATE flight SET crew_list_id = '[15, 16]' WHERE id = 9;
 UPDATE flight SET crew_list_id = '[17, 18]' WHERE id = 10;
